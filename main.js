c54c61fe5b9a0f9395fc8f6896891aa1caddcf0f
yossef13 <yossef.g.09@gmail.com>
2022-04-09 10:39:13 +0300

    new file

--- main.js
+++ main.js
@@ -1,2 +1,45 @@
-// alert ("hello");
-// alert ("hi");
+const cards = ["🍔","🍕","🍩","🍿","🌯","🎂","🍔","🍕","🍩","🍿","🌯","🎂",]
+
+function creatcards (i) {
+    const cardEl = document.createElement("div");
+    cardEl.innerHTML = cards[i];
+    cardEl.id = i;
+    return cardEl;
+}
+
+let newCards = shuffle(cards);
+console.log(newCards);
+
+for (i in newCards) {
+    const element = creatcards(i);
+    board.appendChild(element)
+}
+
+// function shufflecard(arr) {
+//     arr.map(value => ({ value, sort: Math.random() }))
+//     return arr.sort()
+// }
+
+
+function shuffle(array) {
+    var j, x, i;
+    for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = array[i];
+        array[i] = array[j];
+        array[j] = x;
+    }
+    return array;
+}
+
+
+document.getElementById("head").onmouseover = function() {mouseOver()};
+document.getElementById("head").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+  document.getElementById("head").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("head").style.color = "black";
+}
